aefc1edc478f016385224293fc7333e4660de277
wallison <wallisongoncalves105ibc@gmail.com>
2021-10-14 19:44:49 -0400

    add axios to request http and build resquest to stormglass

---
+++ src/clients/__test__/stormGlass.test.ts
@@ -0,0 +1,20 @@
+import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
+
+describe('StormGlass client', () => {
+  it('should return the normalized forecast from the stormGlass service', async () => {
+    const lat = -33.7927216;
+    const lng = 151.2545445;
+
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+
+    const stormGlass = new StormGlass(axios);
+    const response = await stormGlass.fetchPoint(lat, lng);
+
+    expect(response).toEqual({});
+  });
+});
