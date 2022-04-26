7a0c6d7107a16e9670c51a8f56369aef48752abf
wallison <wallisongoncalves105ibc@gmail.com>
2022-04-25 22:17:42 -0400

    added user to model Beach

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,39 +1,54 @@
-import { Beach, BeachPosition } from "@src/models/beach";
-import stormGlassWeather3HoursFixture from "@test/fixtures/stormglass_weather_3_hours.json";
-import apiForecastResponse1BeachFixture from '@test/fixtures/api_forecast_response_1_beach.json'
+import { Beach, BeachPosition } from '@src/models/beach';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import apiForecastResponse1BeachFixture from '@test/fixtures/api_forecast_response_1_beach.json';
 import nock from 'nock';
+import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 
 describe('Beach forecast functional tests', () => {
-  beforeEach(async() => {
+  const defaultUser = {
+    name: 'Jhon Doe',
+    email: 'jhon2@email.com',
+    password: '1234',
+  };
+  let token: string;
+
+  beforeEach(async () => {
     await Beach.deleteMany({});
+    await User.deleteMany({});
+
+    const user = await new User(defaultUser).save();
+    token = AuthService.generateToken(user.toJSON());
     const defaultBeach = {
       lat: -33.792726,
       lng: 151.289824,
       name: 'Manly',
-      position: BeachPosition.E
+      position: BeachPosition.E,
+      user: user,
     };
-    const beach = new Beach(defaultBeach);
-    await beach.save();
+    await new Beach(defaultBeach).save();
   });
 
   it('should return a forecast with just a few times', async () => {
     nock('https://api.stormglass.io:443', {
-      "encodedQueryParams":true,
+      encodedQueryParams: true,
       reqheaders: {
-        Authorization: (): boolean => true
-      }
+        Authorization: (): boolean => true,
+      },
     })
-    .defaultReplyHeaders({'access-control-allow-origin': '*'})
-    .get('/v2/weather/point')
-    .query({
-      "lat":"-33.792726",
-      "lng":"151.289824",
-      "params": /(.*)/,
-      "source":"noaa"
-    })
-    .reply(200, stormGlassWeather3HoursFixture)
+      .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+      .get('/v2/weather/point')
+      .query({
+        lat: '-33.792726',
+        lng: '151.289824',
+        params: /(.*)/,
+        source: 'noaa',
+      })
+      .reply(200, stormGlassWeather3HoursFixture);
 
-    const { body, status } = await global.testRequest.get('/forecast');
+    const { body, status } = await global.testRequest
+      .get('/forecast')
+      .set({ 'x-access-token': token });
     expect(status).toBe(200);
     expect(body).toEqual(apiForecastResponse1BeachFixture);
   });
@@ -50,7 +65,9 @@ describe('Beach forecast functional tests', () => {
       .query({ lat: '-33.792726', lng: '151.289824' })
       .replyWithError('Something went wrong');
 
-    const { status } = await global.testRequest.get(`/forecast`);
+    const { status } = await global.testRequest
+      .get(`/forecast`)
+      .set({ 'x-access-token': token });
 
     expect(status).toBe(500);
   });
